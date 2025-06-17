1b1880953b568b448284a1384aa93c3e057c2d04
Omar Rampado <o.rampado@student.vu.nl>
2025-06-17 10:17:12 +0200

    Assignment 2: cleaned code

--- assignment_2/baseline.py
+++ assignment_2/baseline.py
@@ -97,13 +97,13 @@ class PreyBase(Agent[BaseConfig]):
     # random_walk
 
     def onAlive(self):
-        #FIXME Add connection to dead and reproduce state
+        #FIXME Add connection to dead
         assert self.state == self.State.ALIVE
 
         self.random_walk()
 
         newState: State = self.state
-         
+
         if self.timer >= self.config.repr_time_prey:
             newState = self.State.REPRODUCE
             self.timer = 0 #Reset timer 
@@ -121,7 +121,7 @@ class PreyBase(Agent[BaseConfig]):
     def onDead(self):
         assert self.state == self.State.DEAD
         newState: State = self.state
-         
+
         return newState
 
     # onDead
@@ -189,11 +189,11 @@ class PredatorBase(Agent[BaseConfig]):
         self.random_walk()
 
         newState: State = self.state
-         
+
         if self.timer >= self.config.death_time_predator:
             newState = self.State.DEAD
             self.timer = 0 #Reset timer
-        
+
         #FIXME remove redundancy here or in hunt
         for agent, _ in self.in_proximity_accuracy():
             if isinstance(agent, PreyBase):
@@ -276,185 +276,20 @@ class PredatorBase(Agent[BaseConfig]):
     # change_position
 
 # PredatorBase
+
 WIDTH = Config().window.width
 HEIGHT = Config().window.height
 
-# Snapshots analysis
-
-def dist(a: tuple[int,int], b: tuple[int,int]):
-    """
-    Minimum distance in the wraparound plane
-    """
-    global WIDTH, HEIGHT
-    W = WIDTH
-    H = HEIGHT
-
-    dx = abs(a[0] - b[0])
-    dx = min(dx, W - dx)
-
-    dy = abs(a[1] - b[1])
-    dy = min(dy, H - dy)
-
-    # euclidean distance
-    return math.sqrt((dx*dx) + (dy*dy))
-# dist
-
-
-def frame_metrics(frame):
-    """
-    frame: a dataframe where 'frame' is constant.
-
-    Return: number of clusters, ecs
-    """
-    global MIN_DIST
-    N = len(frame) # number of agents
-
-    G = nx.Graph()
-    G.add_nodes_from(range(N))
-
-    for i in range(0, N):
-        xi = frame.item(row=i, column="x")
-        yi = frame.item(row=i, column="y")
-
-        for j in range(i + 1, N):
-            xj = frame.item(row=j, column="x")
-            yj = frame.item(row=j, column="y")
-
-            d: float = dist((xi,yi),(xj,yj))
-            if d < MIN_DIST:
-                G.add_edge(i,j)
-
-    # Expected Cluster Size
-    # for each agent (node) get the size of the cluster where it belongs
-    sizes: list[int] = [0]*N
-    clusters: int = 0
-    for cluster in nx.connected_components(G):
-        clusters += 1
-        # cluster is a set
-        for node in cluster:
-            # every node must be in only one cluster
-            assert sizes[node] == 0
-            # squared to give more weight to big clusters
-            sizes[node] = len(cluster) ** 2
-
-    ecs: float = math.sqrt(sum(sizes) / N)
-
-    return clusters, ecs
-# frame_metrics
-
-# Analysis
-
-def collect_metrics(df, rate=60):
-    """
-    Create a dataframe with a row for every rate frames in df with the metrics
-    """
-    M = df["frame"].max()
-
-    frames: list = []
-    ecsList: list = []
-    clustersList: list = []
-
-    for i in range(0, M+1, rate):
-
-        snap = df.filter(df["frame"] == i)
-        clusters, ecs = frame_metrics(snap)
-
-        frames.append(i)
-        ecsList.append(ecs)
-        clustersList.append(clusters)
-
-    df = pl.DataFrame({
-        "frame": frames,
-        "ECS": ecsList,
-        "Clusters": clustersList,
-    })
-
-    return df
-# collect_metrics
-
-def run_simulation(simulation):
-    #FIXME add more configs
-    """
-    return metrics
-    """
-    global WIDTH, HEIGHT
-    df = (
-        simulation
-        .batch_spawn_agents(args.predator_amount, PredatorBase, images=["images/red_triangle.png"])
-        .batch_spawn_agents(args.prey_amount, PreyBase, images=["images/triangle.png"])
-        .run()
-        .snapshots
-    )
-    return collect_metrics(df)
-# run_simulation
-# FIXME Add more configs,
 conf = BaseConfig(image_rotation=True,
                         fps_limit = args.frame_limit,
                         movement_speed=0.3,
                         radius=args.radius,
                         seed=args.seed if args.seed else None, # for repeatibility
                         duration=60 * args.duration)
-if args.tests:
-#########################################################3
-#
-#        Deprecated
-#
-#########################################################
-    print("TEST MODE. Running {args.tests} simulations")
-    simulation = HeadlessSimulation(conf)
-    print(f"Simulation 001")
-    metrics = run_simulation(simulation)
-    metrics = metrics.rename({"ECS":"ESC_001",
-                              "Clusters": "Clusters_001"})
-
-    for i in range(1, int(args.tests)):
-        simIdStr = f"{i+1:03d}"
-        print(f"Simulation {simIdStr}")
-        conf.seed = i*10 # otherwise it repeats always the same simulation
-        simulation = HeadlessSimulation(conf)
-        mtr = run_simulation(simulation)
-        metrics = metrics.join(mtr, on="frame")
-        metrics = metrics.rename({"ECS": f"ESC_{simIdStr}",
-                                  "Clusters": f"Clusters_{simIdStr}"})
-    # for
-
-    fname: str = f"simulations_T{args.tests}_J{args.join_n}.parquet"
-    metrics.write_parquet(fname)
-    print(f"DONE. Saved on {fname}")
-else:
-
-    # Standalone, visual simulation
-    simulation = Simulation(conf)
-    run_simulation(simulation)
-
-#########################################################3
-#
-#        Deprecated
-#
-#########################################################
-#     fname = f"D{args.duration:.4f}_R{args.radius}_S{args.seed}"
-
-#     # Plotting
-#     plt.figure(figsize=(12, 8))
-
-#     plt.subplot(2, 1, 1)
-#     plt.plot(metrics["frame"], metrics["ECS"], 'b-', label='ECS')
-#     plt.xlabel('Frame')
-#     plt.ylabel('ECS')
-#     plt.title(f'Expected Cluster Size')
-#     plt.ylim(0, metrics["ECS"].max())
-#     plt.grid(True)
-#     plt.legend()
-
-#     plt.subplot(2, 1, 2)
-#     plt.plot(metrics["frame"], metrics["Clusters"], 'r-', label='Clusters')
-#     plt.xlabel('Frame')
-#     plt.ylabel('Clusters')
-#     plt.title(f'Number of Clusters')
-#     plt.ylim(0, metrics["Clusters"].max()+1)
-#     plt.grid(True)
-#     plt.legend()
-
-#     plt.tight_layout()
-#     plt.savefig("plots/"+fname+".png", dpi=300, bbox_inches='tight')
-# # if test
+df = (
+    Simulation(conf)
+    .batch_spawn_agents(args.predator_amount, PredatorBase, images=["images/red_triangle.png"])
+    .batch_spawn_agents(args.prey_amount, PreyBase, images=["images/triangle.png"])
+    .run()
+#    .snapshots
+)
